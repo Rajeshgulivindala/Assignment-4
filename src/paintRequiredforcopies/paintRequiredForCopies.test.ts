52a452c146a161bd1669a1e32c919e892ddb5b49
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-04-09 15:23:38 -0400

    Test: Added testcase to Calculate  for 6x4 artwork with 1000 copies

--- src/paintRequiredforcopies/paintRequiredForCopies.test.ts
+++ src/paintRequiredforcopies/paintRequiredForCopies.test.ts
@@ -5,6 +5,8 @@ describe('paintRequiredForCopies', () => {
     test('calculates for 3x2 artwork with 5000 copies', () => {
       expect(paintRequiredForCopies(3, 2, 5000)).toBe(4000);
     });
-    
+    test('calculates for 6x4 artwork with 1000 copies', () => {
+        expect(paintRequiredForCopies(6, 4, 1000)).toBe(3200);
+      });
 });
 });
