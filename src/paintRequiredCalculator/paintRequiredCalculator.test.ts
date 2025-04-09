7d8e84e242eca4028d92c64470fa71b13b74c07d
Rajesh <rgulivindala1146@conestoga.on.ca>
2025-04-09 15:43:14 -0400

    Added testcase to Paint Required Calculator handling decimal values

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -9,4 +9,8 @@ describe("paint required calculator", () => {
   test('returns 0 when area is 0', () => {
     expect(paintRequiredCalculator(0, 10)).toBe(0);
   });
+  test('handles fractional area values', () => {
+    expect(paintRequiredCalculator(75.5, 10)).toBeCloseTo(7.55);
+  });
+
 });
